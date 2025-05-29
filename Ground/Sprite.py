4c20c0838204be2908326262969e8be6fb2492fb
LovIMarK <mark@lovink.ch>
2025-05-29 15:44:13 +0200

    cache the asset to be more optimal

--- Ground/Sprite.py
+++ Ground/Sprite.py
@@ -1,18 +1,17 @@
-from Var import *
-
+import os
+from settings import *
 
 def flip(sprites):
     return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
 
-
 def load_sprite_sheets(dir1, dir2, width, height, direction=False):
-    path = join("assets", dir1, dir2)
-    images = [f for f in listdir(path) if isfile(join(path, f))]
+    path = os.path.join("assets", dir1, dir2)
+    images = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
     all_sprites = {}
 
     for image in images:
-        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+        sprite_sheet = pygame.image.load(os.path.join(path, image)).convert_alpha()
 
         sprites = []
         for i in range(sprite_sheet.get_width() // width):
@@ -29,26 +28,26 @@ def load_sprite_sheets(dir1, dir2, width, height, direction=False):
 
     return all_sprites
 
-def get_block(sizeX,sizeY,location):
-    path = join("assets", "Terrain", "Terrain.png")
+def get_block(sizeX, sizeY, location):
+    path = os.path.join("assets", "Terrain", "Terrain.png")
     image = pygame.image.load(path).convert_alpha()
     surface = pygame.Surface((sizeX, sizeY), pygame.SRCALPHA, 32)
-    rect = pygame.Rect(location[0],location[1], sizeX, sizeY)
+    rect = pygame.Rect(location[0], location[1], sizeX, sizeY)
     surface.blit(image, (0, 0), rect)
     return pygame.transform.scale2x(surface)
 
-def get_text(size,location,color):
-    if color=="white":
-        path = join("assets", "Menu\Text", "Text (White) (8x10).png")
-    elif color=="black":
-        path = join("assets", "Menu\Text", "Text (Black) (8x10).png")
+def get_text(size, location, color):
+    if color == "white":
+        path = os.path.join("assets", "Menu", "Text", "Text (White) (8x10).png")
+    elif color == "black":
+        path = os.path.join("assets", "Menu", "Text", "Text (Black) (8x10).png")
+    else:
+        raise ValueError(f"Color not recognized: {color}")
     image = pygame.image.load(path).convert_alpha()
     surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
-    rect = pygame.Rect(location[0],location[1], size, size)
+    rect = pygame.Rect(location[0], location[1], size, size)
     surface.blit(image, (0, 0), rect)
     return pygame.transform.scale2x(surface)
 
-
-
 def Flip(obj):
-    pygame.transform.flip(obj,False,False)
+    pygame.transform.flip(obj, False, False)
--- Traps/Spikes.py
+++ Traps/Spikes.py
@@ -1,36 +1,37 @@
-from Var import *
+from settings import *
 from Objects import Object
 
-
 class Spikes(Object):
-    
-
-    def __init__(self, x, y, width, height,direction):
+    def __init__(self, x, y, width, height, direction="up"):
         super().__init__(x, y, width, height, "spikes")
-        self.spikes = load_sprite_sheets("Traps", "Spikes", width, height)
-        self.image = self.spikes["Idle"][0]
-        self.mask = pygame.mask.from_surface(self.image)
-        self.direction=direction
-
-    def on(self):
-        match self.direction:
-            case "right":
-                self.rotateRight()
-            case "left":
-                self.rotateLeft()
-            case "down":
-                self.rotateDown()
-
-         
-
-    def rotateRight(self):
-        self.image = pygame.transform.rotate(self.image, 270)
+        self.sprites = load_sprite_sheet_cached("Traps", "Spikes", width, height)
+        self.animation_name = "Idle"
+        self.animation_count = 0
+        self.direction = direction
+        self.set_oriented_images()
         self.mask = pygame.mask.from_surface(self.image)
 
-    def rotateLeft(self):
-        self.image = pygame.transform.rotate(self.image, 90)
+    def set_oriented_images(self):
+        base_images = self.sprites[self.animation_name]
+        self.oriented_images = []
+        for img in base_images:
+            if self.direction == "right":
+                oriented_img = pygame.transform.rotate(img, 270)
+            elif self.direction == "left":
+                oriented_img = pygame.transform.rotate(img, 90)
+            elif self.direction == "down":
+                oriented_img = pygame.transform.rotate(img, 180)
+            else:  
+                oriented_img = img
+            self.oriented_images.append(oriented_img)
+        self.image = self.oriented_images[0]
         self.mask = pygame.mask.from_surface(self.image)
-    
-    def rotateDown(self):
-        self.image = pygame.transform.rotate(self.image, 180)
+
+    def loop(self):
+        sprite_index = (self.animation_count) % len(self.oriented_images)
+        self.image = self.oriented_images[sprite_index]
         self.mask = pygame.mask.from_surface(self.image)
+        self.animation_count += 1
+
+    def draw(self, win, offset_x=0, offset_y=0):
+        win.blit(self.image, (self.rect.x - offset_x, self.rect.y - offset_y))
