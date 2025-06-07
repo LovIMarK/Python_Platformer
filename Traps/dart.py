47b360fbf803e4258328c685a22b1114db90f20e
LovIMarK <mark@lovink.ch>
2025-06-07 09:30:21 +0200

    add coments and change name dar -> dart

---
+++ Traps/dart.py
@@ -0,0 +1,31 @@
+from Utils.settings import load_sprite_sheet_cached
+from Core.Objects import Object
+import pygame
+
+class Dart(Object):
+    """
+    Represents a projectile (dart) shot by the Bee enemy.
+    """
+
+    def __init__(self, x, y, width, height):
+        """
+        Initialize the dart.
+
+        Args:
+            x (int): X-coordinate.
+            y (int): Y-coordinate.
+            width (int): Width of the dart sprite.
+            height (int): Height of the dart sprite.
+        """
+        super().__init__(x, y, width, height, "dart")
+        self.sprites = load_sprite_sheet_cached("Enemies", "Bee", width, height)
+        self.image = self.sprites["Bullet"][0]
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self):
+        """
+        Move the dart downward and update its hitbox and mask.
+        """
+        self.rect.y += 5
+        self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.image)
