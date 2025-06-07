c34e02bb8dda6478cf88e10b0e3210a4527449fa
LovIMarK <mark@lovink.ch>
2025-06-07 09:28:19 +0200

    add comments

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+"""
+Entry point for the platformer game.
+
+This module starts the game by calling the main launch function.
+"""
+
 from Core.game import launch
 
 if __name__ == "__main__":
     launch()
-
