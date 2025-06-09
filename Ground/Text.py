efa065c2ebb1c5edb6a663f84f15e27eb183bd67
LovIMarK <mark@lovink.ch>
2025-06-09 20:51:33 +0200

    comments + better variable name

--- Ground/Text.py
+++ Ground/Text.py
@@ -1,10 +1,24 @@
-from Utils.settings import *
-from Core.Objects import Object
+from Utils.settings import get_text_tile
+from Core.objects import Object
+import pygame
 
 class Text(Object):
+    """
+    Represents a text element rendered from a tile-based font system.
+    """
 
-    def __init__(self, x, y, size,location,color="black"):
-        super().__init__(x, y, size, size,"text")
-        text = get_text(size,location,color)
-        self.image.blit(text, (0, 0))
+    def __init__(self, x, y, size, location, color="black"):
+        """
+        Initialize a text object at a given position.
+
+        Args:
+            x (int): X-coordinate for the text.
+            y (int): Y-coordinate for the text.
+            size (int): Size of each character tile.
+            location (tuple): Location of the character in the tile sheet.
+            color (str, optional): Text color (default is "black").
+        """
+        super().__init__(x, y, size, size, "text")
+        text_surface = get_text_tile(size, location, color)
+        self.image.blit(text_surface, (0, 0))
         self.mask = pygame.mask.from_surface(self.image)
