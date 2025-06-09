9e4eb7f39b41823cc236113526543d6cb2bb6e92
LovIMarK <mark@lovink.ch>
2025-06-09 20:54:12 +0200

    comments + better variable name

--- Object/Cube.py
+++ Object/Cube.py
@@ -1,68 +1,93 @@
-from Utils.settings import *
-from Core.Objects import Object
+import pygame
+from Utils.settings import FPS, load_sprite_sheet_cached
+from Core.objects import Object
 
 class Cube(Object):
+    """Represents an interactive cube platform with animations."""
+
     ANIMATION_DELAY = 5
 
     def __init__(self, x, y, width, height):
+        """
+        Initialize a Cube object.
+
+        Args:
+            x (int): X position.
+            y (int): Y position.
+            width (int): Width of the cube.
+            height (int): Height of the cube.
+        """
         super().__init__(x, y, width, height, "cube")
-        self.platforms = load_sprite_sheet_cached("Traps", "Blocks", width, height)
-        self.image = self.platforms["Idle"][0]
+        self.platform_sprites = load_sprite_sheet_cached("Traps", "Blocks", width, height)
+        self.image = self.platform_sprites["Idle"][0]
         self.mask = pygame.mask.from_surface(self.image)
-        self.animation_count = 0
-        self.animation_name = "Idle"
-        self.hit=False
-        self.hit_count=0
-        self.invisible=False
-        self.invisible_count=0
 
+        self.animation_frame = 0
+        self.current_animation = "Idle"
+        
+        self.is_hit = False
+        self.hit_timer = 0
+        self.is_invisible = False
+        self.invisible_timer = 0
 
-    def Invisible(self):
-        self.animation_name = "Invisible (22x22)"
-        self.hit=False
-        self.hit_count=0
+    def set_invisible(self):
+        """Set cube to invisible state."""
+        self.current_animation = "Invisible (22x22)"
+        self.is_hit = False
+        self.hit_timer = 0
 
-    def HitTop(self):
-        self.animation_name = "HitTop (22x22)"
-        self.hit=True
-        self.invisible=True
+    def hit_top(self):
+        """Handle cube top collision."""
+        self.current_animation = "HitTop (22x22)"
+        self.is_hit = True
+        self.is_invisible = True
+        self.hit_timer = 0
 
-    def HitSide(self):
-        self.animation_name = "HitSide (22x22)"
+    def hit_side(self):
+        """Handle cube side collision."""
+        self.current_animation = "HitSide (22x22)"
 
-    def Visible(self):
-        self.animation_name = "Idle"
-        self.invisible=False
-        self.invisible_count=0
-    
-    def Hit1(self):
-        self.animation_name = "Part 1 (22x22)"
-        self.hit1=True
-        self.hit=False
-        self.hit_count=0
-        self.rect.y+=20
+    def set_visible(self):
+        """Set cube back to visible (idle) state."""
+        self.current_animation = "Idle"
+        self.is_invisible = False
+        self.invisible_timer = 0
 
-    def Hit2(self):
-        self.animation_name = "Part 2 (22x22)"
-        self.rect.y-=20    
+    def break_part_1(self):
+        """First stage of cube break animation."""
+        self.current_animation = "Part 1 (22x22)"
+        self.is_hit = False
+        self.hit_timer = 0
+        self.rect.y += 20
 
+    def break_part_2(self):
+        """Second stage of cube break animation."""
+        self.current_animation = "Part 2 (22x22)"
+        self.rect.y -= 20
 
-    def loop(self):
-        sprites = self.platforms[self.animation_name]
-        sprite_index = (self.animation_count //
-                        self.ANIMATION_DELAY) % len(sprites)
+    def update(self):
+        """Update cube's animation and state every frame."""
+        sprites = self.platform_sprites[self.current_animation]
+        sprite_index = (self.animation_frame // self.ANIMATION_DELAY) % len(sprites)
+
         self.image = sprites[sprite_index]
-        self.animation_count += 1
-        self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
         self.mask = pygame.mask.from_surface(self.image)
-        if  self.hit:
-            self.hit_count+=1
-            if self.hit_count>FPS/4:
-                self.Invisible()
-        if  self.invisible:
-            self.invisible_count+=1
-            if self.invisible_count>FPS*2:
-                self.Visible()
-            
-        if self.animation_count // self.ANIMATION_DELAY > len(sprites):
-            self.animation_count = 0
+        self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
+
+        self.animation_frame += 1
+
+        # Handle hit state duration
+        if self.is_hit:
+            self.hit_timer += 1
+            if self.hit_timer > FPS / 4:
+                self.set_invisible()
+
+        # Handle invisibility duration
+        if self.is_invisible:
+            self.invisible_timer += 1
+            if self.invisible_timer > FPS * 2:
+                self.set_visible()
+
+        # Reset animation frame if cycle complete
+        if self.animation_frame // self.ANIMATION_DELAY >= len(sprites):
+            self.animation_frame = 0
--- Object/Fruits.py
+++ Object/Fruits.py
@@ -1,41 +1,52 @@
-from Utils.settings import *
-from Core.Objects import Object
+from Utils.settings import FPS, load_sprite_sheet_cached
+from Core.objects import Object
+import pygame
 
 class Fruit(Object):
     ANIMATION_DELAY = 3
 
-    def __init__(self, x, y, width, height,kind):
+    def __init__(self, x, y, width, height, kind):
+        """
+        Initialize a collectible fruit object.
+
+        Args:
+            x (int): X-coordinate of the fruit.
+            y (int): Y-coordinate of the fruit.
+            width (int): Width of the fruit sprite.
+            height (int): Height of the fruit sprite.
+            kind (str): Type of fruit (e.g., 'Apple', 'Bananas').
+        """
         super().__init__(x, y, width, height, "fruit")
-        self.fruit = load_sprite_sheet_cached("Items", "Fruits", width, height)
-        self.image = self.fruit["Apple"][0]
-        self.mask = pygame.mask.from_surface(self.image)
-        self.animation_count = 0
+        self.fruit_sprites = load_sprite_sheet_cached("Items", "Fruits", width, height)
         self.animation_name = kind
-        self.collected=False
-        self.collected_count=0
-        self.exist=True
+        self.image = self.fruit_sprites[self.animation_name][0]
+        self.mask = pygame.mask.from_surface(self.image)
 
+        self.animation_count = 0
+        self.collected = False
+        self.collected_count = 0
+        self.exist = True
 
-    def Collected(self):
+    def collect(self):
+        """Trigger the fruit collection animation."""
         self.animation_name = "Collected"
-        self.collected=True
-            
-    
-        
+        self.collected = True
 
-    def loop(self):
-        sprites = self.fruit[self.animation_name]
-        sprite_index = (self.animation_count //
-                        self.ANIMATION_DELAY) % len(sprites)
+    def update(self):
+        """Update fruit animation state each frame."""
+        sprites = self.fruit_sprites[self.animation_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
         self.image = sprites[sprite_index]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
+
         self.animation_count += 1
 
-        self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
-        self.mask = pygame.mask.from_surface(self.image)
-
-        if self.animation_count // self.ANIMATION_DELAY > len(sprites):
+        if self.animation_count // self.ANIMATION_DELAY >= len(sprites):
             self.animation_count = 0
-        if  self.collected:
-            self.collected_count+=1
-        if  self.collected_count>FPS/3.2:
-            self.exist=False
+
+        if self.collected:
+            self.collected_count += 1
+
+        if self.collected_count > FPS / 3.2:
+            self.exist = False
